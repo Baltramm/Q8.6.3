6a8db6cee735e1d5708057f67052c272238e3efa
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-26 02:18:58 +0600

    Добавьте файлы проекта.

---
+++ Q8.6.3/DirectorySize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Q8._6._3
+{
+    public static class DirectorySize
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] files = d.GetFiles();
+            foreach (FileInfo fi in files)
+            {
+                size += fi.Length;
+            }
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+
+            }
+            return size;
+        }
+    }
+}
---
+++ Q8.6.3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Q8._6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C://Filex";
+
+
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    var directory = new DirectoryInfo(filePath);
+                    Console.WriteLine("Исходный размер в байтах:{0}", DirectorySize.DirSize(directory));
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    var directory = new DirectoryInfo(filePath);
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+
+                        var deltaTime = DateTime.Now - File.GetLastWriteTime(filePath); ;
+                        if (deltaTime == TimeSpan.FromMinutes(30))
+                        {
+                            file.Delete();
+                        }
+
+                    }
+                    foreach (DirectoryInfo direc in directory.GetDirectories())
+                    {
+                        var deltaTime = DateTime.Now - File.GetLastWriteTime(filePath);
+                        if (deltaTime == TimeSpan.FromMinutes(30))
+                        {
+                            direc.Delete(true);
+                        }
+
+
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
